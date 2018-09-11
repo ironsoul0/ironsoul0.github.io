7d9bcf9bc74e820068fc92349bf03b9f7f9a7662
T1mka <timka2609@gmail.com>
2018-09-11 14:19:00 +0600

    made the header position responsive

--- src/components/AboutMe/AboutFirstScreen/AboutFirstScreen.js
+++ src/components/AboutMe/AboutFirstScreen/AboutFirstScreen.js
@@ -1,34 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './AboutFirstScreen.css';
 import Typist from 'react-typist';
 
-const AboutFirstScreen = (props) => {
-  return (
-    <div className={styles.container}>
-      <div className={styles.mainPart}>
-        <Typist avgTypingDelay={110} className={styles.typist_name} cursor={{ element: '_', blink: true, show: true }}>
-          <h1 className={styles.name} style={{ color: '#552FED' }}>
-            i'm&nbsp;
+const HEADER_HEIGHT = 120;
+
+class AboutFirstScreen extends Component {
+  state = {
+    height: 0
+  }
+
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener('resize', this.updateWindowDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions);
+  }
+
+  updateWindowDimensions() {
+    this.setState({ height: window.innerHeight });
+  }
+
+  render() {
+    const containerStyle = {
+      height: this.state.height - HEADER_HEIGHT
+    };
+
+    return (
+      <div className={styles.container} style={containerStyle}>
+        <div className={styles.mainPart}>
+          <Typist avgTypingDelay={110} className={styles.typist_name} cursor={{ element: '_', blink: true, show: true }}>
+            <h1 className={styles.name} style={{ color: '#552FED' }}>
+              i'm&nbsp;
           </h1>
-          <h1 className={styles.name}>
-            motivated
+            <h1 className={styles.name}>
+              motivated
           </h1>
-          <Typist.Backspace count={9} delay={300} />
-          <h1 className={styles.name}>
-            ambitious
+            <Typist.Backspace count={9} delay={300} />
+            <h1 className={styles.name}>
+              ambitious
           </h1>
-          <Typist.Backspace count={9} delay={300} />
-          <h1 className={styles.name}>
-            energetic
+            <Typist.Backspace count={9} delay={300} />
+            <h1 className={styles.name}>
+              energetic
           </h1>
-          <Typist.Backspace count={9} delay={300} />
-          <h1 className={styles.name}>
-            ironsoul
+            <Typist.Backspace count={9} delay={300} />
+            <h1 className={styles.name}>
+              ironsoul
           </h1>
-        </Typist>
+          </Typist>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default AboutFirstScreen;
